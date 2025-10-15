e6c0b7f1e2e08f2e7e5be432144f373b93764793
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2025-10-15 18:02:52 -0400

    pass app_magnitude_test

--- server/app.py
+++ server/app.py
@@ -34,5 +34,15 @@ def earthquake_by_id(id):
 
     return make_response(body, status)
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_magnitude(magnitude):
+    quakes = []
+    for earthquake in Earthquake.query.filter(Earthquake.magnitude>=magnitude).all():
+        quakes.append(earthquake.to_dict())
+    body = {'count': len(quakes),
+            'quakes': quakes
+            }
+    return make_response(body, 200)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
