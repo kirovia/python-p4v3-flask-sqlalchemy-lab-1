76fcb71d0df27ed111ff1d8eb12721e3ff40a5cb
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2025-10-15 17:33:34 -0400

    pass app_earthquake_test

--- server/app.py
+++ server/app.py
@@ -21,7 +21,18 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id==id).first()
 
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
